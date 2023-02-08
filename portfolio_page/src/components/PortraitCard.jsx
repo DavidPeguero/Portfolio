d983359457d6d6ca7e2ef4330c448315226073b4
David Peguero <77859066+DavidPeguero@users.noreply.github.com>
2023-02-08 18:23:54 -0500

    Changed text in description

--- portfolio_page/src/components/PortraitCard.jsx
+++ portfolio_page/src/components/PortraitCard.jsx
@@ -6,15 +6,9 @@ export default function PortraitCard(props){
             <div className='card--text-container'>
                 <h2>Software Developer | Full Stack</h2>
                 <h1>David Peguero</h1>
-                <h3>I am dedicated developer who wants to learn lots
-                I am dedicated developer who wants to learn lots
-                I am dedicated developer who wants to learn lots
-                I am dedicated developer who wants to learn lots
-                I am dedicated developer who wants to learn lots
-                I am dedicated developer who wants to learn lots
-                I am dedicated developer who wants to learn lots
-             
-                
+                <h3>I am dedicated developer who has experience in both Frontend and Backend 
+                    experience. I have expereience in game design and site design throughout my career
+                    as a developer. I am currently working to finish my Bachelor's at the University of Florida. 
                 </h3>
             </div>
             <img className='card--portrait' src={"" || props.img}></img>
