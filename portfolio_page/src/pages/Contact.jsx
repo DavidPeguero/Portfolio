1b67ccfecbb29236709b259fad6c862ea4c1d09c
David Peguero <77859066+DavidPeguero@users.noreply.github.com>
2023-02-09 14:25:37 -0500

    Fixed contact link for GitHub

--- portfolio_page/src/pages/Contact.jsx
+++ portfolio_page/src/pages/Contact.jsx
@@ -7,7 +7,7 @@ export default function Contact(){
                 <ul>
                     <li>Email: david.peguero.cancel@gmail.com</li>
                     <li>LinkedIn : <a href="https://www.linkedin.com/in/david-peguero">david-peguero</a></li>
-                    <li>Github: <a href="github.com/DavidPeguero">DavidPeguero</a></li>
+                    <li>Github: <a href="https://www.github.com/DavidPeguero">DavidPeguero</a></li>
                 </ul>
             </div>
         </div>
